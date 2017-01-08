f035b59329d3ea85c9f576092f7800bc0d717780
5unagawa <5unagawa@Home>
2017-01-08 22:28:36 +1000

    Connects to server and has basic commands. Will post a sick picture of a VL when I ask it to.

---
+++ BabbysFirstBot/BabbysFirstBot/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+
+namespace BabbysFirstBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+
+        private DiscordClient _client;
+
+        public void Start()
+        {
+            _client = new DiscordClient(x =>
+            {
+                x.AppName = "Babby's First Bot";
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            _client.UsingCommands(x =>
+            {
+                x.PrefixChar = '!'; //prefix needed to activate bot commands
+                x.HelpMode = HelpMode.Public;
+            });
+
+            CreateCommands();
+
+            var token = "MjY1ODMzNTI1Nzg0MDg0NDgw.C1OQrA.tldP0yAwWOgGTYrsnPEiuMzyC3Y";
+
+            //Connect Bot to Server
+            _client.ExecuteAndWait(async () =>
+            {
+                await _client.Connect(token, TokenType.Bot);
+            });
+        }
+
+        public void CreateCommands()
+        {
+            var cService = _client.GetService<CommandService>();
+            cService.CreateCommand("ping")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("What do you want?");
+                });
+
+            cService.CreateCommand("dose")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("vl.jpg");
+                    await e.Channel.SendMessage("tsu tsu tsu");
+                });
+        }
+
+        public void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine($"[{e.Severity}] [{e.Source}] {e.Message}");
+            // [INFO] [Discord] Client Connected
+        }
+    }
+}
